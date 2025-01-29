79098b4505aa54074dda879ae59b9d69897395b8
nullpainter <nullpainter@seznam.cz>
2025-01-29 15:34:21 +0100

    Cleanup ... split stuff up more ...

--- LNZ.cs
+++ LNZ.cs
@@ -16,11 +16,11 @@ class LNZ
 
             if (line.StartsWith("[Eyes]"))
             {
-                ParseList(lines, Eyes, ref lineIndex);
+                LnzParser.ParseList(lines, Eyes, ref lineIndex);
             }
             else if (line.StartsWith("[Paint Ballz]"))
             {
-                ParseList(lines, PaintBallz, ref lineIndex);
+                LnzParser.ParseList(lines, PaintBallz, ref lineIndex);
             }
              // else if (line.StartsWith("[Paint Ballz]"))
              // {
@@ -51,51 +51,4 @@ class LNZ
         }
     }
 
-    private void ParseList<T>(string[] lines, List<T> outList, ref int lineIndex) where T : class, new()
-    {
-        ForeachRowInSection((row) =>
-        {
-            var item = LnzAttributeParser.FromLine<T>(row);
-            if (item != null)
-            {
-                outList.Add(item);
-            }
-        }, lines, ref lineIndex);
-    }
-    
-    void ForeachRowInSection(Action<string> LineCallback, string[] lines, ref int lineIndex)
-    {
-        // next line
-        lineIndex++;
-
-        while (lineIndex < lines.Length)
-        {
-            string line = lines[lineIndex].Trim();
-
-            // Console.WriteLine("Parsing item ... '" + lines[lineIndex] + '"');
-
-            // skip empty lines
-            bool isEmptyLine = line.Length == 0;
-            if (isEmptyLine)
-            {
-                lineIndex++;
-
-                continue;
-            }
-
-            bool nextEntry = line[0] == '[';
-            if (nextEntry)
-            {
-                return; // all items are parsed, keep the current line index
-            }
-
-            bool isComment = line[0] == ';' || line[0] == '#' || line.StartsWith("//");
-            if (!isComment)
-            {
-                LineCallback(line);
-            }
-
-            lineIndex++; // next line
-        }
-    }
 };
---
+++ parsing/LnzParser.cs
@@ -0,0 +1,52 @@
+﻿using main.parsing;
+
+public class LnzParser
+{
+    public static void ParseList<T>(string[] lines, List<T> outList, ref int lineIndex) where T : class, new()
+    {
+        ForeachRowInSection((row) =>
+        {
+            var item = LnzAttributeParser.FromLine<T>(row);
+            if (item != null)
+            {
+                outList.Add(item);
+            }
+        }, lines, ref lineIndex);
+    }
+    
+    static void ForeachRowInSection(Action<string> LineCallback, string[] lines, ref int lineIndex)
+    {
+        // next line
+        lineIndex++;
+
+        while (lineIndex < lines.Length)
+        {
+            string line = lines[lineIndex].Trim();
+
+            // Console.WriteLine("Parsing item ... '" + lines[lineIndex] + '"');
+
+            // skip empty lines
+            bool isEmptyLine = line.Length == 0;
+            if (isEmptyLine)
+            {
+                lineIndex++;
+
+                continue;
+            }
+
+            bool nextEntry = line[0] == '[';
+            if (nextEntry)
+            {
+                return; // all items are parsed, keep the current line index
+            }
+
+            bool isComment = line[0] == ';' || line[0] == '#' || line.StartsWith("//");
+            if (!isComment)
+            {
+                LineCallback(line);
+            }
+
+            lineIndex++; // next line
+        }
+    }
+}
